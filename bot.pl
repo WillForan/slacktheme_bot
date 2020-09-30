ff13a3c16862e2194331bb75b0f2e65d560dfce9
Will Foran <willforan@gmail.com>
2020-09-29 20:32:15 -0400

    💥 switch to theme reminder instead of setter

--- bot.pl
+++ bot.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/env perl
-use strict; use warnings; use feature qq/say/;
+use strict; use warnings; use feature qw/say signatures/;
+no warnings qw/experimental::signatures/;
 #
 # send a giphy url for a given theme to the random slack channel
 # - if "manual-theme.txt" has been updated in the last day. theme will be pulled from that
@@ -10,41 +11,90 @@ use strict; use warnings; use feature qq/say/;
 # posts json to https://slack.com/api/chat.postMessage to send message
 # WebService::Slack::WebApi is a heavy depend to run auth and send message
 
-# 20200925WF - init
-use WebService::Slack::WebApi;
+# 20200925WF - init. send a giphy to 'random'
+# 20200929WF - send a reminder to a person for them to set a theme.
+
+package GiphyTheme;
 use File::Slurp;
 use URI::Escape;
+
+sub get_theme(){
+   #system('git pull'); # update maybe
+  my $man_fname = "manual-theme.txt";
+  my $is_manual = ( -s $man_fname and -M $man_fname < 1);
+  
+  my $theme = $is_manual ? qx/sed 1q $man_fname/ : qx/shuf -n 1 theme_list.txt/;
+  my $theme_note = $is_manual ? "manual" : "automatic";
+  chomp($theme);
+  return($theme_note, $theme);
+}
+
+
+sub get_giphy($theme){
+  chomp(my $giphy_key = read_file('.giphy'));
+  my $giphy_search = "https://api.giphy.com/v1/gifs/search?api_key=".
+                     "$giphy_key&q=".
+                     uri_escape($theme).
+                     "&offset=0&limit=10";
+  chomp(my $img_url = qx/
+     curl -qL "$giphy_search" |
+     jq -r '.data[] |
+            select(.images.original.size|tonumber| . <= 1000000)| .url' |
+     shuf -n1/);
+  return $img_url;
+}
+sub slack_text($img_url, $theme, $prefix="") {
+  # prefix previously like "today's note theme: "
+  my $have_img = $img_url =~ m/http/;
+  my $txt = $have_img?"$prefix<$img_url|$theme>": "no giphy for *$theme*! :scream:";
+  return($txt)
+}
+
+sub giphy_text() {
+   my ($note, $theme) = get_theme();
+   my $img_url = get_giphy($theme);
+   return slack_text($img_url, $theme);
+}
+
+
+package Slack;
+use WebService::Slack::WebApi;
+use File::Slurp;
+sub slack_login() {
+   chomp(my $slack_token = read_file('.oauth'));
+   my $slack = WebService::Slack::WebApi->new(token => $slack_token) or die "no slack! $!";
+   return($slack);
+}
+
+use Class::Tiny { auth => &slack_login };
+sub msg($self, $txt, $to="random") {
+    # to can be a person (e.g. @name) or channel (e.g. random)
+    # posting message to specified channel and getting message description
+    my $posted_message = $self->auth->chat->post_message(
+         channel  => $to,
+         text     => "$txt",
+         link_names=>1,
+    );
+    return($posted_message);
+}
+
+
+package main;
 use Data::Dumper;
+use File::Slurp;
 
-# read in api key/tokens
-chomp(my $slack_token = read_file('.oauth'));
-chomp(my $giphy_key = read_file('.giphy'));
+sub get_setter(){
+  # will needs to go first on 20200929. so set offset to 3
+  my @everyone = read_file('ids.txt', chomp=>1);
+  my $day_of_year = (localtime)[7];
+  my $setter = $everyone[$day_of_year % ($#everyone+1)];
+  return $setter;
+}
 
 
-# find theme
-system('git pull'); # update maybe
-my $man_fname = "manual-theme.txt";
-my $is_manual = ( -s $man_fname and -M $man_fname < 1);
-
-my $theme = $is_manual ? qx/sed 1q $man_fname/ : qx/shuf -n 1 theme_list.txt/;
-my $theme_note = $is_manual ? "manual" : "automatic";
-chomp($theme);
-
-# pull image url from giphy
-my $giphy_search = "https://api.giphy.com/v1/gifs/search?api_key=$giphy_key&q=".uri_escape($theme)."&offset=0&limit=10";
-chomp(my $img_url = qx/curl -qL "$giphy_search" | jq -r '.data[] | select(.images.original.size|tonumber| . <= 1000000)| .url'|shuf -n1/);
-my $have_img = $img_url =~ m/http/;
-my $txt = $have_img?"today's $theme_note theme: <$img_url|$theme>": "no giphy for *$theme*! :scream:";
-
-# init slack
-my $slack = WebService::Slack::WebApi->new(token => $slack_token) or die "no slack! $!";
-
-my $edit_note = "; <https://github.com/LabNeuroCogDevel/slacktheme_bot/edit/master/manual-theme.txt|set tomorrow's theme>";
-# posting message to specified channel and getting message description
-my $posted_message = $slack->chat->post_message(
-    channel  => 'random', # required
-    #channel  => 'random', # required
-    text     => "$txt $edit_note",       # required (not required if 'attachments' argument exists)
-);
-
-#say Dumper($posted_message);
+my $setter = "@".get_setter();
+my $giphy_txt = GiphyTheme::giphy_text();
+# my $edit_note = "; <https://github.com/LabNeuroCogDevel/slacktheme_bot/edit/master/manual-theme.txt|set tomorrow's theme>";
+my $slack = Slack->new;
+my $resp = $slack->msg("<$setter> sets the theme next!", 'random');
+$resp = $slack->msg("It's your turn to set the theme tomorrow. Here's some motivation: $giphy_txt", '@will');
